69a070876a61d8e5f3706a933c00594924e12c8e
fakhrul <fakhrul.alam@btracsl.com>
2021-10-31 17:33:34 +0600

    -geocoder

--- app/src/main/java/com/fakhrulasa/geolocator/MapsActivity.kt
+++ app/src/main/java/com/fakhrulasa/geolocator/MapsActivity.kt
@@ -1,30 +1,26 @@
 package com.fakhrulasa.geolocator
 
 import android.Manifest
-import android.content.Context
 import android.content.pm.PackageManager
 import android.content.res.Resources
-import android.location.Location
-import androidx.appcompat.app.AppCompatActivity
+import android.location.*
 import android.os.Bundle
 import android.util.Log
 import android.view.Menu
 import android.view.MenuItem
+import androidx.appcompat.app.AppCompatActivity
 import androidx.core.app.ActivityCompat
 import androidx.core.content.ContextCompat
-
+import com.fakhrulasa.geolocator.databinding.ActivityMapsBinding
 import com.google.android.gms.maps.CameraUpdateFactory
 import com.google.android.gms.maps.GoogleMap
 import com.google.android.gms.maps.OnMapReadyCallback
 import com.google.android.gms.maps.SupportMapFragment
 import com.google.android.gms.maps.model.LatLng
-import com.google.android.gms.maps.model.MarkerOptions
-import com.fakhrulasa.geolocator.databinding.ActivityMapsBinding
 import com.google.android.gms.maps.model.MapStyleOptions
+import com.google.android.gms.maps.model.MarkerOptions
+import com.google.android.material.snackbar.Snackbar
 import java.util.*
-import android.location.LocationListener
-import android.location.LocationManager
-import android.widget.Toast
 
 
 class MapsActivity : AppCompatActivity(), OnMapReadyCallback, LocationListener {
@@ -33,10 +29,10 @@ class MapsActivity : AppCompatActivity(), OnMapReadyCallback, LocationListener {
     private lateinit var binding: ActivityMapsBinding
     private val TAG = MapsActivity::class.java.simpleName
     private val REQUEST_LOCATION_PERMISSION = 1
-    var latitude:Double=0.0
-    var longitude:Double=0.0
+    var latitude: Double = 0.0
+    var longitude: Double = 0.0
 
-    private lateinit var mLocationManager:LocationManager
+    private lateinit var mLocationManager: LocationManager
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
@@ -71,11 +67,14 @@ class MapsActivity : AppCompatActivity(), OnMapReadyCallback, LocationListener {
 
 
     }
-    private fun isPermissionGranted() : Boolean {
+
+    private fun isPermissionGranted(): Boolean {
         return ContextCompat.checkSelfPermission(
             this,
-            Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED
+            Manifest.permission.ACCESS_FINE_LOCATION
+        ) == PackageManager.PERMISSION_GRANTED
     }
+
     private fun enableMyLocation() {
         if (isPermissionGranted()) {
             if (ActivityCompat.checkSelfPermission(
@@ -96,8 +95,7 @@ class MapsActivity : AppCompatActivity(), OnMapReadyCallback, LocationListener {
                 return
             }
             map.isMyLocationEnabled = true
-        }
-        else {
+        } else {
             ActivityCompat.requestPermissions(
                 this,
                 arrayOf<String>(Manifest.permission.ACCESS_FINE_LOCATION),
@@ -105,13 +103,16 @@ class MapsActivity : AppCompatActivity(), OnMapReadyCallback, LocationListener {
             )
         }
     }
+
     private val mLocationListener: LocationListener = LocationListener {
-        latitude=it.latitude
-        longitude=it.longitude
+        latitude = it.latitude
+        longitude = it.longitude
         val mapFragment = supportFragmentManager
             .findFragmentById(R.id.map) as SupportMapFragment
         mapFragment.getMapAsync(this)
+        getAddress()
     }
+
     /**
      * Manipulates the map once available.
      * This callback is triggered when the map is ready to be used.
@@ -123,19 +124,19 @@ class MapsActivity : AppCompatActivity(), OnMapReadyCallback, LocationListener {
      */
     override fun onMapReady(googleMap: GoogleMap) {
         map = googleMap
-
+        setMapStyle(map)
         //These coordinates represent the latitude and longitude of the Googleplex.
-
         val zoomLevel = 15f
 
         val homeLatLng = LatLng(latitude, longitude)
         map.moveCamera(CameraUpdateFactory.newLatLngZoom(homeLatLng, zoomLevel))
         map.addMarker(MarkerOptions().position(homeLatLng))
         setMapLongClick(map)
-        setMapStyle(map)
+
         enableMyLocation()
 
     }
+
     private fun setMapLongClick(map: GoogleMap) {
         map.setOnMapLongClickListener { latLng ->
             // A Snippet is Additional text that's displayed below the title.
@@ -162,24 +163,48 @@ class MapsActivity : AppCompatActivity(), OnMapReadyCallback, LocationListener {
         return true
     }
 
-    override fun onOptionsItemSelected(item: MenuItem)=when(item.itemId) {
-           R.id.normal_map -> {
-               map.mapType = GoogleMap.MAP_TYPE_NORMAL
-               true
-           }
-           R.id.hybrid_map -> {
-               map.mapType = GoogleMap.MAP_TYPE_HYBRID
-               true
-           }
-           R.id.satellite_map -> {
-               map.mapType = GoogleMap.MAP_TYPE_SATELLITE
-               true
-           }
-           R.id.terrain_map -> {
-               map.mapType = GoogleMap.MAP_TYPE_TERRAIN
-               true
-           }
-           else -> super.onOptionsItemSelected(item)
+    fun getAddress() {
+        val geocoder: Geocoder
+        val addresses: List<Address>
+        geocoder = Geocoder(this, Locale.getDefault())
+
+        addresses = geocoder.getFromLocation(
+            latitude,
+            longitude,
+            1
+        ) // Here 1 represent max location result to returned, by documents it recommended 1 to 5
+
+
+        val address: String =
+            addresses[0].getAddressLine(0) // If any additional address line present than only, check with max available address lines by getMaxAddressLineIndex()
+        var snackbar: Snackbar = Snackbar.make(binding.root, address, Snackbar.LENGTH_INDEFINITE)
+        snackbar.setAction("close") {
+            snackbar.dismiss()
+        }
+        snackbar.setActionTextColor(ContextCompat.getColor(this, R.color.white))
+        snackbar.setTextColor(ContextCompat.getColor(this, R.color.white))
+        snackbar.setBackgroundTint(ContextCompat.getColor(this, R.color.snackbarcolor))
+        snackbar.show()
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem) = when (item.itemId) {
+        R.id.normal_map -> {
+            map.mapType = GoogleMap.MAP_TYPE_NORMAL
+            true
+        }
+        R.id.hybrid_map -> {
+            map.mapType = GoogleMap.MAP_TYPE_HYBRID
+            true
+        }
+        R.id.satellite_map -> {
+            map.mapType = GoogleMap.MAP_TYPE_SATELLITE
+            true
+        }
+        R.id.terrain_map -> {
+            map.mapType = GoogleMap.MAP_TYPE_TERRAIN
+            true
+        }
+        else -> super.onOptionsItemSelected(item)
     }
 
     private fun setMapStyle(map: GoogleMap) {
